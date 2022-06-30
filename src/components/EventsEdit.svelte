87acf21125e45c6314a1289744dc53fac9b4b7ce
Alexander Eversbusch <alexandereversbusch@AlexandersMBP2.attlocal.net>
2022-06-30 01:40:51 -0400

    Lots of styling on events pages.

--- src/components/EventsEdit.svelte
+++ src/components/EventsEdit.svelte
@@ -6,6 +6,7 @@
 	import { user } from '../stores/authStore.js';
 	import { onMount } from 'svelte';
 	import { eventLabel } from '../stores/eventStore.js';
+	import { titleize } from '../helpers/titleize.js';
 
 	const handleChecked = (event_id) => {
 		toggleBelongsToUser(event_id, $user.id);
@@ -37,12 +38,103 @@
 	<form class="my-6">
 		<div class="flex flex-col text-sm mb-2">
 			{#each $events as show}
-			<label style="text-transform: capitalize;" class="font-bold mb-2 text-gray-800" for="event-checkbox">
-				<input class="mt-1 mr-1" id="event-checkbox" type="checkbox" bind:group={shows}
-					on:click={handleChecked(show.id)} value={JSON.stringify(show)} data-event-id={show.id} />
-				{eventLabel(show)}
-			</label>
+				<label
+					style="text-transform: capitalize;"
+					class="font-bold mb-2 text-gray-800"
+					for="event-checkbox"
+				>
+					<input
+						class="mt-1 mr-1"
+						id="event-checkbox"
+						type="checkbox"
+						bind:group={shows}
+						on:click={handleChecked(show.id)}
+						value={JSON.stringify(show)}
+						data-event-id={show.id}
+					/>
+					{eventLabel(show)}
+				</label>
 			{/each}
 		</div>
 	</form>
 </div>
+
+<!-- <div class="px-4 sm:px-6 lg:px-8">
+	<div class="sm:flex sm:items-center">
+		<div class="sm:flex-auto">
+			<h1 class="text-xl font-semibold text-gray-900">My Shows</h1>
+			<p class="mt-2 text-sm text-gray-700">All of the shows you've attended</p>
+		</div>
+		<div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+			<a href="/events/edit">
+				<button
+					type="button"
+					class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+					>Update My Shows</button
+				>
+			</a>
+		</div>
+	</div>
+	<div class="-mx-4 mt-10 ring-1 ring-gray-300 sm:-mx-6 md:mx-0 md:rounded-lg">
+		<table class="min-w-full divide-y divide-gray-300">
+			<thead>
+				<tr>
+					<th
+						scope="col"
+						class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 inline-flex"
+					>
+						Date
+					</th>
+					<th
+						scope="col"
+						class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+						>Venue
+					</th>
+					<th
+						scope="col"
+						class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+						>City
+					</th>
+					<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+						>State</th
+					>
+				</tr>
+			</thead>
+			<tbody>
+				{#if $events.length > 0}
+					{#each $events as show}
+						<tr>
+							<td class="relative py-4 pl-4 sm:pl-6 pr-3 text-sm">
+								<div class="font-medium text-gray-900">{show.date.replaceAll('-', '.')}</div>
+								<div class="mt-1 flex flex-col text-gray-500 sm:block lg:hidden">
+									<span>{titleize(show.venue)}</span>
+								</div>
+							</td>
+
+							<td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+								>{titleize(show.venue)}</td
+							>
+
+							<td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+								>{titleize(show.city)}</td
+							>
+
+							<td class="px-3 py-3.5 text-sm text-gray-500">
+								<div class="lg:block hidden">
+									{show.state.toUpperCase()}
+								</div>
+								<div class="md:block lg:hidden">
+									{titleize(show.city)} · {show.state.toUpperCase()}
+								</div>
+							</td>
+						</tr>
+					{/each}
+				{:else}
+					<p class="text-lg text-center text-gray-800 mt-4">
+						Get started by selecting all of the shows you've attended 💃
+					</p>
+				{/if}
+			</tbody>
+		</table>
+	</div>
+</div> -->
--- src/components/EventsIndex.svelte
+++ src/components/EventsIndex.svelte
@@ -1,24 +1,106 @@
 <script>
-  import { userEvents, eventLabel, loadUserEvents } from '../stores/eventStore.js'
-  import { user } from '../stores/authStore.js'
-  import { onMount } from 'svelte'
+	import { userEvents, eventLabel, loadUserEvents } from '../stores/eventStore.js';
+	import { user } from '../stores/authStore.js';
+	import { onMount } from 'svelte';
+	import { titleize } from '../helpers/titleize.js';
 
-  onMount(async () => {
-    await loadUserEvents($user.id)
-  })
+	onMount(async () => {
+		await loadUserEvents($user.id);
+	});
 </script>
 
-<div class="container mx-auto my-6 max-w-lg">
-  <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">My shows</h1>
-  <ul class="text-left mt-5">
-    {#if $userEvents.length > 0}
-    {#each $userEvents as userEvent}
-    <li style="text-transform: capitalize;">{eventLabel(userEvent)}</li>
-    {/each}
-    {:else}
-    <p class="text-lg text-center text-gray-800 mt-4">
-      Get started by selecting all of the shows you've attended 💃
-    </p>
-    {/if}
-  </ul>
+<div class="px-4 sm:px-6 lg:px-8">
+	<div class="sm:flex sm:items-center">
+		<div class="sm:flex-auto">
+			<h1 class="text-xl font-semibold text-gray-900">My Shows</h1>
+			<p class="mt-2 text-sm text-gray-700">All of the shows you've attended</p>
+		</div>
+		<div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+			<a href="/events/edit">
+				<button
+					type="button"
+					class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+					>Update My Shows</button
+				>
+			</a>
+		</div>
+	</div>
+	<div class="-mx-4 mt-10 ring-1 ring-gray-300 sm:-mx-6 md:mx-0 md:rounded-lg">
+		<table class="min-w-full divide-y divide-gray-300">
+			<thead>
+				<tr>
+					<th
+						scope="col"
+						class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 inline-flex"
+						>Date
+						<!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
+						<!-- <span class="ml-7 flex-none rounded bg-gray-200 text-gray-900 group-hover:bg-gray-300"> -->
+						<!-- Heroicon name: solid/chevron-down -->
+						<!-- <svg
+								class="h-5 w-5"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</span> -->
+					</th>
+					<th
+						scope="col"
+						class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+						>Venue
+					</th>
+					<th
+						scope="col"
+						class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+						>City
+					</th>
+					<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+						>State</th
+					>
+				</tr>
+			</thead>
+			<tbody>
+				{#if $userEvents.length > 0}
+					{#each $userEvents as userEvent}
+						<tr>
+							<td class="relative py-4 pl-4 sm:pl-6 pr-3 text-sm">
+								<div class="font-medium text-gray-900">{userEvent.date.replaceAll('-', '.')}</div>
+								<div class="mt-1 flex flex-col text-gray-500 sm:block lg:hidden">
+									<span>{titleize(userEvent.venue)}</span>
+								</div>
+							</td>
+
+							<td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+								>{titleize(userEvent.venue)}</td
+							>
+
+							<td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+								>{titleize(userEvent.city)}</td
+							>
+
+							<td class="px-3 py-3.5 text-sm text-gray-500">
+								<div class="lg:block hidden">
+									{userEvent.state.toUpperCase()}
+								</div>
+								<div class="md:block lg:hidden">
+									{titleize(userEvent.city)} · {userEvent.state.toUpperCase()}
+								</div>
+							</td>
+						</tr>
+					{/each}
+				{:else}
+					<p class="text-lg text-center text-gray-800 mt-4">
+						Get started by selecting all of the shows you've attended 💃
+					</p>
+				{/if}
+			</tbody>
+		</table>
+	</div>
 </div>
